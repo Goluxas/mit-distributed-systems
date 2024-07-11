ed28afcc9c774754cdeedf604db81c128d90d76d
Doug Storz <douglasstorz@gmail.com>
2024-07-11 04:02:25 -0400

    Worker asks for task and maps given files

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,28 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"os"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -36,13 +31,55 @@ func Worker(mapf func(string, string) []KeyValue,
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+	for {
+		// Ask coordinator for a task
+		getTaskArgs := &GetTaskArgs{}
+		getTaskReply := &GetTaskReply{}
+		ok := call("Coordinator.GetTask", &getTaskArgs, &getTaskReply)
+		if !ok || getTaskReply.Error != "" {
+			log.Fatal("Task failed! RPC Error: ", getTaskReply.Error)
+		}
+
+		// Execute assigned task
+		if getTaskReply.TaskType == "MAP" {
+			mapFiles(mapf, getTaskReply.InputFiles, getTaskReply.NReduce)
+		} else if getTaskReply.TaskType == "REDUCE" {
+			reduceFiles(reducef, getTaskReply.InputFiles)
+		}
+
+	}
 }
 
-//
+func mapFiles(mapf func(string, string) []KeyValue, inputFiles []string, nReduce int) {
+	kvs := []KeyValue{}
+	for _, filename := range inputFiles {
+		contents, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatal("Failed to open file:", filename)
+		}
+		newKvs := mapf(filename, string(contents))
+		kvs = append(kvs, newKvs...) // join two slices with ... expansion
+
+	}
+	writeMapOutput(kvs, nReduce)
+}
+
+func writeMapOutput(kvs []KeyValue, nReduce int) {
+	// open temp output files for writing
+
+	// TEMPORARY
+	for kv := range kvs {
+		println(kv)
+	}
+}
+
+func reduceFiles(reducef func(string, []string) string, inputFiles []string) {
+
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +104,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
